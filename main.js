e7e3dafc6ed285e4ab595d73a319d396d3bbd7bc
John Chang <johnchang9094@gmail.com>
2017-04-27 20:40:16 +0100

    React Native Exponent Set Up

---
+++ main.js
@@ -0,0 +1,18 @@
+import Expo from 'expo';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Todo extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+
+        };
+    }  
+
+    render() {
+
+    }
+}
+
+Expo.registerRootComponent(Todo);
